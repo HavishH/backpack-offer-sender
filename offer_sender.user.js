7496c3b8dc3d59d1ac9573c15419a6ad801b595a
Hav <None>
2024-10-27 15:19:20 -0300

    v.1.3.3

--- offer_sender.user.js
+++ offer_sender.user.js
@@ -2,7 +2,7 @@
 // @name         One-Click Offer
 // @namespace    https://github.com/HavishH/backpack-offer-sender
 // @homepage     https://github.com/HavishH
-// @version      1.3.2
+// @version      1.3.3
 // @description  Adds a button on backpack.tf listings that instantly sends the offer.
 // @author       Brom127
 // @updateURL    https://github.com/HavishH/backpack-offer-sender/raw/main/offer_sender.user.js
